bf872149f1f3ba9dc13e3235d4c7c4311510e66b
aziz sebihi <219293567+azizsebihi@users.noreply.github.com>
2025-07-19 14:15:49 +0200

    3

--- backend/src/server.js
+++ backend/src/server.js
@@ -19,11 +19,8 @@ const app = express();
 
 app.use(morgan("common"));
 
-app.get("/", function(req, res, next) {
-  database.raw('select VERSION() version')
-    .then(([rows, columns]) => rows[0])
-    .then((row) => res.json({ message: `Hello from MySQL ${row.version}` }))
-    .catch(next);
+app.get("/", function(req, res) {
+  res.send("Hello Docker World\n");
 });
 
 app.get("/healthz", function(req, res) {
